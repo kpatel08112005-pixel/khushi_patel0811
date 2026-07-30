24e7f92f7a533c98c5ae31fe107bc305a1d8159e
kpatel08112005-pixel <kpatel08112005@gmail.com>
2026-07-30 06:23:40 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all JS code as never version
+
+//alert(3 + 3) //we are using nodedjs, not browser 
+
+console.log(3
+    +
+    3) //code readablity should be high
+
+console.log("Khushi")
+
+let name = "Khushi"
+let age = "20"
+let isLoggedIn = false 
+let stete;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// bollean => true/false
+// null => standalone value
+// indifine =>
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); //undefine
+console.log(typeof null); //object
